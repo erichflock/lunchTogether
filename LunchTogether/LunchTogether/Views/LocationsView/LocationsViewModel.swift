0878541c046ac04bd1b2bef6bf10230ed49b0aa3
Erich Flock <erich.flock@check24.de>
2022-06-24 11:57:32 +0200

    Empty locations when fetching new content

--- LunchTogether/LunchTogether/Views/LocationsView/LocationsViewModel.swift
+++ LunchTogether/LunchTogether/Views/LocationsView/LocationsViewModel.swift
@@ -12,6 +12,7 @@ class LocationsViewModel: ObservableObject {
     @Published var locations: [Location] = []
     
     func getLocations() {
+        locations.removeAll()
         let location1 = Location(city: "München", office: "Brücke")
         let location2 = Location(city: "Leipzig", office: "Brühl")
         let location3 = Location(city: "Berlin", office: "Alexanderplatz")
