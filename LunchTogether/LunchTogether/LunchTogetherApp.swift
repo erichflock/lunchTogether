1072cff7befee4368e744f551add2dcae439fc87
Erich Flock <erich.flock@check24.de>
2022-06-24 11:05:54 +0200

    Create project

---
+++ LunchTogether/LunchTogether/LunchTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchTogetherApp.swift
+//  LunchTogether
+//
+//  Created by Erich Flock on 24.06.22.
+//
+
+import SwiftUI
+
+@main
+struct LunchTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
